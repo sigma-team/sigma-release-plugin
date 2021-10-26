f96d314e9244ff14cad206a103ce9ce2b52f6be2
Alexander Lashkovsky <qweszxc_777>
2021-10-26 20:58:52 +0300

    SIGMA postRelease

--- src/main/kotlin/com/sigma/domain/ReleaseManager.kt
+++ src/main/kotlin/com/sigma/domain/ReleaseManager.kt
@@ -2,6 +2,8 @@ package com.sigma.domain
 
 import org.eclipse.jgit.api.Git
 
+const val MASTER = "master"
+
 fun preBuild(git: Git, releaseBranch: String, preReleaseCommitMessage: String, bootstrapFile: String) {
     addSpringCloudConfigLabel(bootstrapFile = bootstrapFile, releaseBranch)
     checkout(git, releaseBranch, createNewBranch = true)
@@ -9,22 +11,26 @@ fun preBuild(git: Git, releaseBranch: String, preReleaseCommitMessage: String, b
     push(git, releaseBranch)
 }
 
-fun postBuild() {
-    //TODO
+fun postBuild(projectGit: Git,
+              cloudConfigGit: Git,
+              tagName: String,
+              preReleaseCommitMessage: String,
+              newVersionCommitMessage: String) {
+    createTagInCloudConfigRepo(cloudConfigGit, tagName, preReleaseCommitMessage)
+    updateProjectVersion(projectGit, newVersionCommitMessage)
 }
 
-fun createTagInCloudConfigRepo() {
-    //TODO
+fun createTagInCloudConfigRepo(git: Git, tagName: String, preReleaseCommitMessage: String) {
+    checkout(git, MASTER)
+    createTag(git, tagName, preReleaseCommitMessage)
+    push(git, tagName)
 }
 
-fun updateVersion() {
-    //TODO
+fun updateProjectVersion(git: Git, newVersionCommitMessage: String) {
+    checkout(git, MASTER)
+    incrementAppVersion()
+    add(git, GRADLE_PROPERTIES_FILE)
+    commit(git, newVersionCommitMessage)
+    push(git, MASTER)
 }
 
-fun createTagInProjectRepo() {
-    //TODO
-}
-
-fun doPreReleaseActions() {
-    //TODO
-}
