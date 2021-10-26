7def3419438aff5aac12e56d4a16af657e214ae0
Maksim Butrim <maksim.butrim@amobee.com>
2021-10-26 15:13:24 +0300

    add git add and commit functions

--- src/main/kotlin/com/sigma/domain/GitCommand.kt
+++ src/main/kotlin/com/sigma/domain/GitCommand.kt
@@ -1,4 +1,26 @@
 package com.sigma.domain
 
-class GitCommand {
-}
+import org.eclipse.jgit.api.Git
+
+
+fun checkout(git: Git,
+             destination: String,
+             createNewBranch: Boolean = false,
+             forceCheckout: Boolean = false
+) = git.checkout()
+        .setName(destination)
+        .setForced(forceCheckout)
+        .setCreateBranch(createNewBranch)
+        .call()
+
+fun commit(git: Git,
+           message: String
+) = git.commit()
+        .setMessage(message)
+        .call()
+
+fun add(git: Git,
+        vararg filePatterns: String) =
+        git.add().apply {
+            filePatterns.forEach { this.addFilepattern(it) }
+        }.call()
