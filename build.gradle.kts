cb621046d0b0624012fa3dcc723607c00545cab9
ilya.gonchar <ilya.gonchar@amobee.com>
2021-10-27 02:14:37 +0300

    - fix #3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    id("com.github.johnrengelman.shadow").version("7.1.0")
     `kotlin-dsl`
     `maven-publish`
 }
@@ -21,15 +22,27 @@ gradlePlugin {
     }
 }
 
+/**
+ * Special configuration to be included in resulting shadowed jar, but not added to the generated pom and gradle
+ * metadata files.
+ */
+val shadowImplementation by configurations.creating
+configurations["compileOnly"].extendsFrom(shadowImplementation)
+configurations["testImplementation"].extendsFrom(shadowImplementation)
+
 dependencies {
     implementation("commons-configuration:commons-configuration:1.10")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
-    implementation ("org.eclipse.jgit:org.eclipse.jgit:5.13.0.202109080827-r")
+    shadowImplementation("org.eclipse.jgit:org.eclipse.jgit:5.13.0.202109080827-r")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 tasks {
+    shadowJar<ShadowJar> {
+        configurations = listOf(shadowImplementation)
+        archiveName = "sigma-team.sigma-release-plugin.jar"
+    }
     jar {
         archiveBaseName.set("sigma-team.sigma-release-plugin")
 
