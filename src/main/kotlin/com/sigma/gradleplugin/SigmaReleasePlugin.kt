3004743a3450a493432a31fbdd568334c43d28d0
ilya.gonchar <ilya.gonchar@amobee.com>
2021-10-26 22:17:41 +0300

    added 'preBuild' and 'postBuild' tasks

--- src/main/kotlin/com/sigma/gradleplugin/SigmaReleasePlugin.kt
+++ src/main/kotlin/com/sigma/gradleplugin/SigmaReleasePlugin.kt
@@ -7,18 +7,47 @@ import org.gradle.kotlin.dsl.register
 
 class SigmaReleasePlugin : Plugin<Project> {
 
+    private val tasksGroup = "sigma release"
+
     override fun apply(rootProject: Project) {
 
-        val extension = rootProject.extensions.create<ReleasePluginExtension>("sigmaReleasePlugin", rootProject.objects)
-
-        rootProject.tasks.register<ReleaseTask>("release") {
-            group = "deploy"
-            description = "TODO"
-
-            ticketNumber.set(extension.ticketNumber)
-            version.set(extension.version)
-            preReleaseCommitMessage.set(extension.preReleaseCommitMessage)
-            newVersionCommitMessage.set(extension.newVersionCommitMessage)
+        val extension = createReleaseExtension(rootProject)
+        val preBuildReleaseTask = createPreBuildReleaseTask(rootProject, extension)
+        val postBuildReleaseTask = createPostBuildReleaseTask(rootProject, extension)
+        print("====tasks" + rootProject.tasks.asMap)
+        rootProject.pluginManager.withPlugin("java-base") {
+            val buildTask = rootProject.tasks.getByName("build")
+            buildTask.dependsOn(preBuildReleaseTask)
+            buildTask.finalizedBy(postBuildReleaseTask)
         }
+    }
+
+    private fun createReleaseExtension(rootProject: Project) =
+        rootProject.extensions.create<ReleasePluginExtension>("sigmaReleasePlugin", rootProject.objects)
+
+    private fun createPreBuildReleaseTask(
+        rootProject: Project,
+        extension: ReleasePluginExtension
+    ) = rootProject.tasks.register<PreBuildReleaseTask>("preBuildRelease") {
+        group = tasksGroup
+        description = "TODO"
+
+        ticketNumber.set(extension.ticketNumber)
+        version.set(extension.version)
+        preReleaseCommitMessage.set(extension.preReleaseCommitMessage)
+        newVersionCommitMessage.set(extension.newVersionCommitMessage)
+    }
+
+    private fun createPostBuildReleaseTask(
+        rootProject: Project,
+        extension: ReleasePluginExtension
+    ) = rootProject.tasks.register<PostBuildReleaseTask>("postBuildRelease") {
+        group = tasksGroup
+        description = "TODO"
+
+        ticketNumber.set(extension.ticketNumber)
+        version.set(extension.version)
+        preReleaseCommitMessage.set(extension.preReleaseCommitMessage)
+        newVersionCommitMessage.set(extension.newVersionCommitMessage)
     }
 }
