ffd286aafcb3d28ff97585379630546e78d51eac
MichaelShavlyuk <michael.shavlyuk@amobee.com>
2021-10-26 22:43:36 +0300

    Remove ReleaseManager class

--- src/main/kotlin/com/sigma/domain/ReleaseManager.kt
+++ src/main/kotlin/com/sigma/domain/ReleaseManager.kt
@@ -2,39 +2,29 @@ package com.sigma.domain
 
 import org.eclipse.jgit.api.Git
 
-class ReleaseManager(
-    var git: Git,
-    val tagName: String,
-    val releaseBranch: String,
-    val preReleaseCommitMessage: String,
-    val newVersionCommitMessage: String,
-    val bootstrapFile: String
-) {
-    fun preBuild() {
-        addSpringCloudConfigLabel(bootstrapFile = bootstrapFile, releaseBranch)
-        checkout(git, releaseBranch, createNewBranch = true)
-        commit(git, preReleaseCommitMessage)
-        push(git, releaseBranch)
-    }
+fun preBuild(git: Git, releaseBranch: String, preReleaseCommitMessage: String, bootstrapFile: String) {
+    addSpringCloudConfigLabel(bootstrapFile = bootstrapFile, releaseBranch)
+    checkout(git, releaseBranch, createNewBranch = true)
+    commit(git, preReleaseCommitMessage)
+    push(git, releaseBranch)
+}
 
-    fun postBuild() {
-        //TODO
-    }
+fun postBuild() {
+    //TODO
+}
 
-    fun createTagInCloudConfigRepo() {
-        //TODO
-    }
+fun createTagInCloudConfigRepo() {
+    //TODO
+}
 
-    fun updateVersion() {
-        //TODO
-    }
+fun updateVersion() {
+    //TODO
+}
 
-    fun createTagInProjectRepo() {
-        //TODO
-    }
+fun createTagInProjectRepo() {
+    //TODO
+}
 
-    fun doPreReleaseActions() {
-        //TODO
-    }
-
+fun doPreReleaseActions() {
+    //TODO
 }
