b5f9dc749b9491a5e0c18c3e7c013f14d20d7b94
ilya.gonchar <ilya.gonchar@amobee.com>
2021-10-27 05:06:10 +0300

    fixed path to project

--- src/main/kotlin/com/sigma/gradleplugin/ReleasePluginExtension.kt
+++ src/main/kotlin/com/sigma/gradleplugin/ReleasePluginExtension.kt
@@ -3,27 +3,27 @@ package com.sigma.gradleplugin
 import org.gradle.api.Project
 import org.gradle.api.model.ObjectFactory
 
-abstract class ReleasePluginExtension(objectFactory: ObjectFactory, rootProject: Project) {
-
-    val projectRepoPath = objectFactory.property(String::class.java)
-        .convention("${rootProject.path}/.git")
-
-    val cloudConfigRepoPath = objectFactory.property(String::class.java)
-        .convention("${rootProject.path}/../config/.git")
+abstract class ReleasePluginExtension(objectFactory: ObjectFactory, project: Project) {
 
     val deploymentTicket = objectFactory.property(String::class.java)
 
+    val projectRepoPath = objectFactory.property(String::class.java)
+        .convention("${project.rootDir.absolutePath}/.git")
+
+    val cloudConfigRepoPath = objectFactory.property(String::class.java)
+        .convention("${project.rootDir.absolutePath}/../config/.git")
+
     val bootstrapFilePath = objectFactory.property(String::class.java)
-        .convention("${rootProject.path}/src/main/resources/bootstrap.yml")
+        .convention("${project.rootDir.absolutePath}/src/main/resources/bootstrap.yml")
 
     val gradlePropertiesFilePath = objectFactory.property(String::class.java)
-        .convention("${rootProject.path}/gradle.properties")
+        .convention("${project.rootDir.absolutePath}/gradle.properties")
 
     val tagName = objectFactory.property(String::class.java)
-        .convention("${rootProject.name}-${rootProject.version}")
+        .convention("${project.name}-${project.version}")
 
     val releaseBranchName = objectFactory.property(String::class.java)
-        .convention("release-${rootProject.version}")
+        .convention("release-${project.version}")
 
     val applicationMainBranch = objectFactory.property(String::class.java)
         .convention("master")
@@ -32,8 +32,8 @@ abstract class ReleasePluginExtension(objectFactory: ObjectFactory, rootProject:
         .convention("master")
 
     val preReleaseCommitMessage = objectFactory.property(String::class.java)
-        .convention("$deploymentTicket: [Release process] Pre release commit, version: '${rootProject.version}'")
+        .convention("$deploymentTicket: [Release process] Pre release commit, version: '${project.version}'")
 
     val newVersionCommitMessage = objectFactory.property(String::class.java)
-        .convention("$deploymentTicket: [Release process] Changed version number to '${rootProject.version}'")
+        .convention("$deploymentTicket: [Release process] Changed version number to '${project.version}'")
 }
